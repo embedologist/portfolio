c8ac91701651006b72198375d697962c4e1d0842
Apple <apple@Apples-MacBook-Pro.local>
2026-07-23 22:06:08 +0530

    embedologist.in-23rd Jul 10.50pm

--- next.config.js
+++ next.config.js
@@ -1,16 +1,10 @@
 /** @type {import('next').NextConfig} */
 
-// This automatically detects if you are running locally or building for GitHub
-const isProd = process.env.NODE_ENV === 'production';
-
 const nextConfig = {
   output: 'export',
   images: {
     unoptimized: true,
   },
-  // Only apply the /portfolio path when deploying to GitHub Pages
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio' : '',
 };
 
 module.exports = nextConfig;
